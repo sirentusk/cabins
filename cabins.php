6128a8fc201ba94ed76dd21b1fc130a22fd3ad8e
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-09 22:51:38 +1100

    Update cabins.php

--- cabins.php
+++ cabins.php
@@ -2,14 +2,14 @@
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Collect the form data
-    $cabinType = $_POST['cabinType'];
+    $cabinType = $_POST['cabinType']; // This will now contain the value from the dropdown
     $description = $_POST['description'];
     $pricePerNight = $_POST['pricePerNight'];
     $pricePerWeek = $_POST['pricePerWeek'];
 
     // Display the submitted data
     echo "<h2>Cabin Information Submitted:</h2>";
-    echo "<p>Cabin Type: $cabinType</p>";
+    echo "<p>Cabin Type: $cabinType</p>"; // Display the selected cabin type
     echo "<p>Description: $description</p>";
     echo "<p>Price Per Night: $pricePerNight</p>";
     echo "<p>Price Per Week: $pricePerWeek</p>";
