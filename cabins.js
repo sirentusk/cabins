887da2b2b2941b0dfe1fd41274d2a341fbaaa480
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-12 23:15:46 +1100

    Update cabins.js

--- cabins.js
+++ cabins.js
@@ -1,1 +1,24 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  const inclusionsSelect = document.getElementById('inclusions');
+  
+  inclusionsSelect.addEventListener('mousedown', (event) => {
+    // Prevent default behavior to allow for multi-selection without holding CTRL
+    event.preventDefault();
+    
+    let option = event.target;
+    
+    if (option.tagName === 'OPTION') {
+      const value = option.value;
+      option.selected = !option.selected;
+      
+      // Trigger a change event since we are preventing the default behavior
+      let changeEvent = new Event('change');
+      inclusionsSelect.dispatchEvent(changeEvent);
+    }
+  }, false);
+  
+  inclusionsSelect.addEventListener('change', (event) => {
+    let selectedOptions = Array.from(event.target.selectedOptions).map(option => option.value);
+    console.log('Selected inclusions:', selectedOptions);
+  });
+});
