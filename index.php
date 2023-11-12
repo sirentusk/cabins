38e99431b3d229d3a0f7cc043046ed371afcf557
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-12 22:55:18 +1100

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,15 @@
 <body>
 
 <form method="post" action="cabins.php">
+
     <label for="cabinType">Cabin Type:</label><br>
     <select id="cabinType" name="cabinType" required>
-        <!-- ... other options ... -->
+        <option value="Standard cabin">Standard cabin – sleeps 4</option>
+        <option value="Standard open plan cabin">Standard open plan cabin – sleeps 4</option>
+        <option value="Deluxe cabin">Deluxe cabin – sleeps 4</option>
+        <option value="Villa">Villa – sleeps 4</option>
+        <option value="Spa villa">Spa villa – sleeps 4</option>
+        <option value="Slab powered site">Slab powered site</option>
     </select><br>
 
     <label for="description">Description:</label><br>
@@ -23,9 +29,20 @@
 
     <label for="inclusions">Cabin Inclusions:</label><br>
     <select id="inclusions" name="inclusions[]" multiple required>
-        <option value="air_conditioner">Air conditioner</option>
-        <option value="linen">Linen</option>
-        <!-- ... more inclusions options ... -->
+        <option value="Air conditioner">Air conditioner</option>
+        <option value="Linen">Linen</option>
+        <option value="Veranda">Veranda</option>
+        <option value="Bunk bed">Bunk bed</option>
+        <option value="Ceiling fans">Ceiling fans</option>
+        <option value="Clock radio">Clock radio</option>
+        <option value="Dining facilities">Dining facilities</option>
+        <option value="Dishwasher">Dishwasher</option>
+        <option value="DVD Player">DVD Player</option>
+        <option value="Foxtel">Foxtel</option>
+        <option value="Fridge/Freezer">Fridge/Freezer</option>
+        <option value="Hair dryer">Hair dryer</option>
+        <option value="Ironing Facilities">Ironing Facilities</option>
+        <option value="Microwave">Microwave</option>
     </select><br>
 
     <input type="submit" value="Submit">
