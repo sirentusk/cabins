c7f16b2aa3a73e608164cfd2f2eb21412733c5e3
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-13 03:47:26 +1100

    Update cabins.js

--- cabins.js
+++ cabins.js
@@ -1,31 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const inclusionsSelect = document.getElementById('inclusions');
-  
-  inclusionsSelect.addEventListener('mousedown', (event) => {
-    // Prevent default behavior to allow for multi-selection in Inclusions
-    event.preventDefault();
-    
-    let option = event.target;
-    
-    if (option.tagName === 'OPTION') {
-      const value = option.value;
-      option.selected = !option.selected;
-      
-      // Allow the new multi-select event
-      let changeEvent = new Event('change');
-      inclusionsSelect.dispatchEvent(changeEvent);
-    }
-  }, false);
 
-  // Save the multi-select options in Inclusions
-  inclusionsSelect.addEventListener('change', (event) => {
-    let selectedOptions = Array.from(event.target.selectedOptions).map(option => option.value);
+  // Allow multi-selection in Inclusions without CTRL key
+  inclusionsSelect.addEventListener('mousedown', (event) => {
+    event.preventDefault(); // Prevent default to enable custom behavior
+
+    const option = event.target;
+    if (option.tagName === 'OPTION') {
+      option.selected = !option.selected; // Toggle the selected state
+
+      // Dispatch a custom 'change' event to update the state
+      const changeEvent = new Event('change', { bubbles: true });
+      option.dispatchEvent(changeEvent);
+    }
+  });
+
+  // Log selected options in Inclusions
+  inclusionsSelect.addEventListener('change', () => {
+    const selectedOptions = Array.from(inclusionsSelect.selectedOptions).map(option => option.value);
     console.log('Selected inclusions:', selectedOptions);
   });
 
-  // Remove "No file chosen" default message by allowing customizing of file inputs
-  document.getElementById('fileInput').addEventListener('change', function() {
-    var fileName = this.files[0].name;
+  // Update custom file label with the selected file name
+  const fileInput = document.getElementById('fileInput');
+  fileInput.addEventListener('change', () => {
+    const fileName = fileInput.files[0] ? fileInput.files[0].name : 'No file chosen';
     document.querySelector('.custom-file-label').textContent = fileName;
   });
 });
