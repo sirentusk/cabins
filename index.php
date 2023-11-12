7c49ee0c12b9b61cdb00579da8abeec911d2a9ac
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-13 00:07:19 +1100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Update Cabin Information</title>
     <script src="cabins.js"></script>
-    <script src="cabins.css"></script>
+    <link rel="stylesheet" type="text/css" href="cabins.css"> 
 </head>
     
 <body>
@@ -49,7 +49,7 @@
     </select><br>
 
     <label for="cabinImage">Cabin Image:</label><br>
-    <input type="file" id="cabinImage" name="cabinImage" accept=".jpg, .jpeg, .png, .tiff, .webp, .svg, .heif, .heic"><br>
+    <input type="file" id="image" name="cabinImage" accept=".jpg, .jpeg, .png, .tiff, .webp, .svg, .heif, .heic"><br>
 
     <input type="submit" value="Submit">
 </form>
