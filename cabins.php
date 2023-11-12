92f029df47f29f5d7f27a62df20c82dbf3c56761
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-12 22:46:39 +1100

    Update cabins.php

--- cabins.php
+++ cabins.php
@@ -1,5 +1,7 @@
 <?php
-// Assuming the database connection is already set up in this file or included from another file
+
+// Database connection setup should be here
+// ...
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -8,26 +10,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $description = $_POST['description'];
     $pricePerNight = $_POST['pricePerNight'];
     $pricePerWeek = $_POST['pricePerWeek'];
-    $inclusions = $_POST['inclusions']; // This is an array of selected inclusions
+    $inclusions = $_POST['inclusions']; // This will be an array of selected inclusions
 
-    // Perform database operations
-    // For example, to insert the form data into the cabins table:
-    $sql = "INSERT INTO cabins (cabin_type, description, price_per_night, price_per_week) VALUES (?, ?, ?, ?)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$cabinType, $description, $pricePerNight, $pricePerWeek]);
+    // Process the form data and interact with the database
+    // ...
 
-    // To insert inclusions, you may have a junction table and need to insert multiple rows
-    // Example:
-    // foreach ($inclusions as $inclusion) {
-    //     $sql_inclusions = "INSERT INTO cabin_inclusions (cabin_id, inclusion_id) VALUES (?, ?)";
-    //     $stmt_inclusions = $pdo->prepare($sql_inclusions);
-    //     // Assuming you retrieve or have the cabin ID and inclusion ID
-    //     $stmt_inclusions->execute([$cabin_id, $inclusion_id]);
-    // }
-
-    // Redirect to a new page or display a success message
+    // Display the submitted data (or redirect to a success page)
     echo "<h2>Cabin Information Submitted:</h2>";
-    // Display the submitted data
-    // ... 
+    echo "<p>Cabin Type: $cabinType</p>";
+    echo "<p>Description: $description</p>";
+    echo "<p>Price Per Night: $pricePerNight</p>";
+    echo "<p>Price Per Week: $pricePerWeek</p>";
+
+    // Handle the inclusions
+    if (!empty($inclusions)) {
+        echo "<h3>Selected Inclusions:</h3>";
+        echo "<ul>";
+        foreach ($inclusions as $inclusion) {
+            echo "<li>" . htmlspecialchars($inclusion) . "</li>";
+        }
+        echo "</ul>";
+    }
 }
+
+// Close the database connection if it's open
+// ...
 ?>
