56eb8e7fcd7ee2982df3a4434e3c8161e4559add
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-13 03:42:26 +1100

    Update cabins.js

--- cabins.js
+++ cabins.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const inclusionsSelect = document.getElementById('inclusions');
   
   inclusionsSelect.addEventListener('mousedown', (event) => {
-    // Prevent default behavior to allow for multi-selection without holding CTRL
+    // Prevent default behavior to allow for multi-selection in Inclusions
     event.preventDefault();
     
     let option = event.target;
@@ -11,14 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
       const value = option.value;
       option.selected = !option.selected;
       
-      // Trigger a change event since we are preventing the default behavior
+      // Allow the new multi-select event
       let changeEvent = new Event('change');
       inclusionsSelect.dispatchEvent(changeEvent);
     }
   }, false);
-  
+
+  // Save the multi-select options in Inclusions
   inclusionsSelect.addEventListener('change', (event) => {
     let selectedOptions = Array.from(event.target.selectedOptions).map(option => option.value);
     console.log('Selected inclusions:', selectedOptions);
+  });
+
+  // Remove "No file chosen" default message by allowing customizing of file inputs
+  document.getElementById('fileInput').addEventListener('change', function() {
+    var fileName = this.files[0].name;
+    document.querySelector('.custom-file-label').textContent = fileName;
   });
 });
