9999f34bc7622414b10bced2f15be692d727c99a
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-15 04:40:24 +1100

    Update cabins.php

--- cabins.php
+++ cabins.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Get the database connection details from environment variables
 $supabaseUrl = getenv('SUPABASE_URL');
 $supabaseKey = getenv('SUPABASE_KEY');
