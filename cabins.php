b82b10f38584e00475f9422f905a5a9234a02b99
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-15 05:22:52 +1100

    Update cabins.php

--- cabins.php
+++ cabins.php
@@ -4,95 +4,67 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Get the database connection details from environment variables
+// Supabase client setup (assuming you have already set this up)
+// require_once 'path/to/supabase-php-sdk/autoload.php';
+// $supabaseClient = new Supabase\SupabaseClient('your-supabase-url', 'your-supabase-key');
+
+// Database connection details
 $supabaseUrl = getenv('SUPABASE_URL');
 $supabaseKey = getenv('SUPABASE_KEY');
 $password = getenv('DB_PASSWORD');
-
-// Parse the URL to get the connection details
 $dbParams = parse_url($supabaseUrl);
 
 $host = 'db.tpogqybedqrbsgjoawxe.supabase.co';
 $dbname = 'postgres';
 $user = 'postgres';
-$password = getenv('DB_PASSWORD');
 $port = '5432';
 
-// Set up the DSN (Data Source Name)
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password;sslmode=require";
+$pdo = new PDO($dsn);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Create a new PDO instance
-try {
-    $pdo = new PDO($dsn);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cabinType = $_POST['cabinType'];
+    $description = $_POST['description'];
+    $pricePerNight = $_POST['pricePerNight'];
+    $pricePerWeek = $_POST['pricePerWeek'];
+    $inclusions = $_POST['inclusions'];
 
-    // Check if the form is submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Collect the form data
-        $cabinType = $_POST['cabinType'];
-        $description = $_POST['description'];
-        $pricePerNight = $_POST['pricePerNight'];
-        $pricePerWeek = $_POST['pricePerWeek'];
-        $inclusions = $_POST['inclusions'];
+    $inclusionsString = '{' . implode(', ', array_map(function($inclusion) {
+        return '"' . addslashes($inclusion) . '"';
+    }, $inclusions)) . '}';
 
-        // Convert the inclusions array to a string
-        $inclusionsString = '{' . implode(', ', array_map(function($inclusion) {
-            return '"' . addslashes($inclusion) . '"';
-        }, $inclusions)) . '}';
+    $sql = "INSERT INTO \"Cabins\" (\"cabinType\", \"Description\", \"pricePerNight\", \"pricePerWeek\", \"Inclusions\") VALUES (?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$cabinType, $description, $pricePerNight, $pricePerWeek, $inclusionsString]);
 
-        // Prepare and execute the SQL statement
-        $sql = "INSERT INTO \"Cabins\" (\"cabinType\", \"Description\", \"pricePerNight\", \"pricePerWeek\", \"Inclusions\") VALUES (?, ?, ?, ?, ?)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$cabinType, $description, $pricePerNight, $pricePerWeek, $inclusionsString]);
-    }
-
-    // Handle the inclusions
-    if (!empty($inclusions)) {
-        echo "<h3>Selected Inclusions:</h3>";
-        echo "<ul>";
-        foreach ($inclusions as $inclusion) {
-            echo "<li>" . htmlspecialchars($inclusion) . "</li>";
-        }
-        echo "</ul>";
-    }
-
-    // Check if a file has been uploaded
     if (isset($_FILES['cabinImage']) && $_FILES['cabinImage']['error'] == UPLOAD_ERR_OK) {
         $image = $_FILES['cabinImage'];
         $imageSize = $_FILES['cabinImage']['size'];
         $imageType = strtolower(pathinfo($image['name'], PATHINFO_EXTENSION));
 
-        // Check if the file size is less than 5MB
-        if ($imageSize > 5000000) { // 5MB in bytes
+        if ($imageSize > 5000000) {
             echo "Error: Image size must be under 5MB.";
         } else {
-            // Check if the file is one of the allowed types
-            $allowedTypes = ['jpg', 'jpeg', 'png', 'tiff', 'webp', 'svg', 'heif', 'heic'];
-            if (in_array($imageType, $allowedTypes)) {
-                $targetDirectory = "uploads/";
+            if (in_array($imageType, ['jpg', 'jpeg', 'png', 'tiff', 'webp', 'svg', 'heif', 'heic'])) {
+                $bucketName = 'cabins';
+                $folder = str_replace(' ', '_', strtolower($cabinType));
                 $newFileName = uniqid('cabin_', true) . '.' . $imageType;
-                $targetFile = $targetDirectory . $newFileName;
+                $filePath = $folder . '/' . $newFileName;
 
-                // Move the uploaded file to the target directory
-                if (move_uploaded_file($image['tmp_name'], $targetFile)) {
-                    echo "Image uploaded successfully.";
-                    // Store the new file name in the database, not the original name from the user
-                    // $sql = "INSERT INTO cabins (image_path) VALUES (?)";
-                    // ... Execute database query to store image path ...
-                } else {
-                    echo "There was an error uploading the image.";
-                }
+                // Supabase upload logic
+                // $response = $supabaseClient->storage()->getBucket($bucketName)->uploadFile($filePath, $image['tmp_name']);
+
+                // if ($response->isSuccess()) {
+                //     echo "Image uploaded successfully to Supabase.";
+                // } else {
+                //     echo "Failed to upload image to Supabase: " . $response->getMessage();
+                // }
             } else {
                 echo "Invalid file type, please upload a jpg, jpeg, png, tiff, webp, svg, heif, or heic file type.";
             }
         }
     }
-} catch (PDOException $e) {
-    // Handle any database connection errors
-    error_log("Connection failed: " . $e->getMessage());
-    die("Connection failed: " . $e->getMessage());
 }
 
-// Close the database connection if it's open
-// ...
 ?>
